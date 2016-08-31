f9aa12733aafa9cf7ed26e1377e439cc9c47f0c7
treylitefm <griffin.omar@gmail.com>
2016-08-31 16:47:10 -0700

    Bubble sort initial commit
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

---
+++ sort.py
@@ -0,0 +1,15 @@
+from random import random
+
+nums = []
+
+for i in range(100): 
+    nums.append(int(random()*100000))
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                (arr[j+1],arr[j]) = arr[j],arr[j+1]
+    return arr
+
+print bubble_sort(nums)
