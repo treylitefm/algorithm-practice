59a10a7a89e081004ac7f0e9e3f8dbb013ba312c
treylitefm <griffin.omar@gmail.com>
2016-09-03 11:52:30 -0700

    Binary Search without recursion (preferred)
    Signed-off-by: treylitefm <griffin.omar@gmail.com>

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
+'''
 def binary_search(arr, target, start=None, n=None):
-    #import ipdb; ipdb.set_trace()
     if start is None:
         start = 0
     if n is None:
@@ -15,8 +15,24 @@ def binary_search(arr, target, start=None, n=None):
         return binary_search(arr, target, i+1, start+(n-1)-i) #search right side
     else: #target < arr[i] by law of trichotomy
         return binary_search(arr, target, start, i-start) #search left side
+'''
+
+def binary_search(arr, target): #implemented without recursion
+    first = 0
+    last = len(arr)-1
+
+    while True:
+        if first > last:
+            return False
+        mid = (first+last)/2
+        if arr[mid] is target:
+            return mid
+        elif arr[mid] > target:
+            last = mid-1
+        else:
+            first = mid+1
 
 def linear_search(arr, n):
     for i in arr:
         if arr[i] is n:
-            return i
+                return i
